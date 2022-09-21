20045e9a47939a9ea34babf403b731373495eedf
Gabe <gb@test.com>
2022-09-20 23:43:45 -0500

    wordfreq

--- WordFrequency.py
+++ WordFrequency.py
@@ -1,6 +1,39 @@
 
 
-with open('sometext.txt','r') as txt:
-    words = txt.read().lower().split(" ")
-    txt.close()
-print(words)
+def main():
+    infile = open('sometext.txt','r')
+    wc = wordcount(infile)
+    print(wc)
+
+def wordcount(infile):
+    wc = {}
+    reader = infile.readlines()
+    
+    for line in reader:
+        words = line.split()
+
+        for w in words:
+            w = w.replace("."," ")
+            
+            if w[-1] == ",":
+                w = w.replace("."," ")
+
+                if w in wc:
+                    wc[w] += 1
+                else:
+                    wc[w] = 1
+
+            if w in wc:
+                wc[w] += 1
+            else:
+                wc[w] = 1
+
+        return wc
+
+    def print(wc):
+        print("Word | Frequency")
+
+        for key in wc:
+            print(key, "--", wc[key])
+
+main()
